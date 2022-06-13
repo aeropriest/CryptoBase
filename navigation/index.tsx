819d13f4c174ad95354591b4cc84e5bbac590608
Ashok <mettaa.games@gmail.com>
2022-06-13 15:10:14 +0800

    first commit

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,7 +3,7 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome, Entypo, AntDesign, FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -12,12 +12,18 @@ import { ColorSchemeName, Pressable } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
+import HomeScreen from '../screens/HomeScreen';
+import MarketScreen from '../screens/MarketScreen';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
+import PortfolioScreen from '../screens/PortfolioScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import RankingScreen from '../screens/RankingScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
+
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
@@ -55,43 +61,57 @@ const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 function BottomTabNavigator() {
   const colorScheme = useColorScheme();
+  const iconSize = 20;
 
   return (
     <BottomTab.Navigator
       initialRouteName="TabOne"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
+      }}>      
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
+        name="Home"
+        component={HomeScreen}
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Entypo name="home" size={iconSize} color={color} />,
         }}
       />
+      <BottomTab.Screen
+        name="Portfolio"
+        component={PortfolioScreen}
+        options={{
+          title: 'Portfolio',
+          tabBarIcon: ({ color }) => <AntDesign name="piechart" size={iconSize-2} color={color} />,
+        }}
+      />
+       <BottomTab.Screen
+        name="Market"
+        component={MarketScreen}
+        options={{
+          title: 'Market',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="chart-line" size={iconSize} color={color} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Rankings"
+        component={RankingScreen}
+        options={{
+          title: 'Rankings',
+          tabBarIcon: ({ color }) => <MaterialIcons name="leaderboard" size={iconSize} color={color} />,
+        }}
+      />
+    <BottomTab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ 
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <FontAwesome name="user" size={iconSize} color={color} />,
+        }}
+      />
+
+
+
     </BottomTab.Navigator>
   );
 }
@@ -103,5 +123,5 @@ function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={iconSize} style={{ marginBottom: -3 }} {...props} />;
 }
---
+++ screens/RankingScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+ 
+const RankingScreen = (props) => {
+    return (  
+        <View style = {{flex: 1, justifyContent: 'center', alignItems:'center'}}>
+            <Text>RankingScreen Component</Text>
+        </View>
+    );
+}
+ 
+export default RankingScreen;
